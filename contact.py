4aea42339d3a0b1026d6ae5d9a353e2562badea2
sungeunCha <cseun999@naver.com>
2020-06-18 14:16:52 +0900

    코드 설명 첨부

--- contact.py
+++ contact.py
@@ -1,3 +1,4 @@
+#Contact 클래스 생성
 class Contact:
     def __init__(self, name, phone_number, e_mail, addr):
         self.name = name
@@ -11,6 +12,7 @@ class Contact:
         print("E-mail: ", self.e_mail)
         print("Address: ", self.addr)
 
+#사용자 정보 입력 함수 - input 함수 사용
 def set_contact():
     name = input("Name: ")
     phone_number = input("Phone Number: ")
@@ -35,27 +37,28 @@ def print_contact(contact_list):
 
 #연락처 삭제 함수
 def delete_contact(contact_list, name):
-    for i, contact in enumerate(contact_list):
-        if contact.name == name:
-            del contact_list[i]
+    for i, contact in enumerate(contact_list): #enumerate: 리스트가 있는 경우 순서와 리스트의 값을 전달
+        if contact.name == name: #contact에 담긴 name이 입력받은 name과 같다면
+            del contact_list[i] #contact_list의 해당 리스트 삭제
 
 #연락처 파일 저장 함수
 def store_contact(contact_list):
-    f = open("contact_db.txt", "wt")
-    for contact in contact_list:
-        f.write(contact.name + '\n')
+    f = open("contact_db.txt", "wt") #contact_db.txt라는 이름의 텍스트 파일 wt모드로 열기
+    for contact in contact_list: #contact_list를 순회하면서 각 인스턴스의 정보 출력
+        f.write(contact.name + '\n') #contact_db.txt 파일에 정보 저장
         f.write(contact.phone_number + '\n')
         f.write(contact.e_mail + '\n')
         f.write(contact.addr + '\n')
     f.close()
 
+#연락처 파일 로드 함수
 def load_contact(contact_list):
     f = open("contact_db.txt", "rt")
-    lines = f.readlines()
+    lines = f.readlines() #readlines 함수를 이용해 파일에 있는 모든 데이터 읽어서 lines에 저장
     num = len(lines) / 4  #연락처 하나당 4줄의 데이터가 존재하므로 4로 나누어 데이터 개수 확인
     num = int(num)  #나눗셈 후 실수가 된 num을 정수형으로 변환
 
-    for i in range(num):
+    for i in range(num): #num의 개수만큼 루프를 돌리면서 lines 리스트에 저장된 데이터를 읽어 들여 Contact 클래스의 인스턴스를 생성하고 생성한 인스턴스를 contact_list에 추가
         name = lines[4*i].rstrip('\n')
         phone = lines[4*i+1].rstrip('\n')
         email = lines[4*i+2].rstrip('\n')
@@ -64,23 +67,24 @@ def load_contact(contact_list):
         contact_list.append(contact)
     f.close()
 
-
+#실행 함수
 def run():
-    contact_list = []
-    load_contact(contact_list)
-    while 1:
-        menu = print_menu()
+    contact_list = [] #리스트 생성
+    load_contact(contact_list) #저장된 파일 로드
+    while 1: #프로그램이 계속 실행되도록 무한루프 사용
+        menu = print_menu() #메뉴 출력
         if menu == 1:
             contact = set_contact()
-            contact_list.append(contact)
+            contact_list.append(contact) #contact_list에 append 함수 사용해서 데이터 붙여넣기
         elif menu == 2:
             print_contact(contact_list)
         elif menu == 3:
             name = input("Name: ")
             delete_contact(contact_list, name)
         elif menu == 4:
-            store_contact(contact_list)
-            break
+            store_contact(contact_list) #리스트를 파일 형태로 저장
+            break #무한루프 종료
 
-if __name__ == "__main__":
+if __name__ == "__main__": #인터프리터에서 직접 실행했을 경우에만 run 함수 실행
+
     run()
